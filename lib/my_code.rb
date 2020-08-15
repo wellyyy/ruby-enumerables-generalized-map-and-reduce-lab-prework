c0a5cf216cf961c720998ac70358f26ec1b3e2fc
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-15 22:27:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,23 @@
-# Your Code Here
+def map(array)
+  newA = []
+  i = 0
+  while i < array.length do
+    newA.push(yield(array[i]))
+    i += 1
+  end
+  newA
+end
+
+def reduce(array)
+ # val = 0
+  #i = 0 
+ # while i < array.length do
+  #  val += yield
+  #  i += 1
+ # end
+ # val
+ yield.all?
+end
+  
+  
+  
