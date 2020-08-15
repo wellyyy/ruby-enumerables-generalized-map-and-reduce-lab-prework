10d6a67d09046266e63e14ee8e82a5eef84240d2
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-15 22:36:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,5 +8,19 @@ def map(array)
   newA
 end
 
+def reduce(array,*starting)
+   if starting[0]
+     value = starting[0]
+     count = 0
+     else
+     value = array[0]
+     count = 1
+   end
 
+    while count < array.count do
+     value =  yield(value,array[count])
+     count+=1
+   end 
+  value
+ end
   
