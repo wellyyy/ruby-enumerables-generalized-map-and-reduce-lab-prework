54580d330b8e7e7b88f233dbb0ccc3d10c10aba4
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-08-15 22:37:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+  newA = []
+  i = 0
+  while i < array.length do
+    newA.push(yield(array[i]))
+    i += 1
+  end
+  newA
+end
+
+def reduce(array,*starting)
+   if starting[0]
+     value = starting[0]
+     count = 0
+     else
+     value = array[0]
+     count = 1
+   end
+
+    while count < array.count do
+     value =  yield(value,array[count])
+     count+=1
+   end 
+  value
+ end
+  
